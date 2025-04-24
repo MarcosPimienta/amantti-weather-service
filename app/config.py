855c75f5145b191ac1497bf3a1efc2781e45dd6a
MarcosPimienta <fenix3819@gmail.com>
2025-04-24 11:46:11 -0500

    Implement async weather data fetch from OpenWeatherMap with temp, humidity, rain, and timestamp

--- app/config.py
+++ app/config.py
@@ -1,1 +1,10 @@
-#config file
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings:
+  WEATHER_API_KEY: str = os.getenv("WEATHER_API_KEY")
+
+def get_settings():
+  return Settings()
