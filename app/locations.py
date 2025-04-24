c3808b3e6c84ed07307c7240cbc32d13df9e69df
MarcosPimienta <fenix3819@gmail.com>
2025-04-23 19:17:24 -0500

    Add list of key coffee-producing towns in Antioquia for weather tracking

---
+++ app/locations.py
@@ -0,0 +1,17 @@
+TOWNS = [
+  {"name": "Jardín", "lat": 5.5982, "lon": -75.8193},
+  {"name": "Andes", "lat": 5.6560, "lon": -75.8770},
+  {"name": "Betania", "lat": 5.7353, "lon": -75.9790},
+  {"name": "Ciudad Bolívar", "lat": 5.8539, "lon": -76.0106},
+  {"name": "Támesis", "lat": 5.6686, "lon": -75.7124},
+  {"name": "Urrao", "lat": 6.3131, "lon": -76.1344},
+  {"name": "Hispania", "lat": 5.8011, "lon": -75.9086},
+  {"name": "Fredonia", "lat": 5.9177, "lon": -75.6725},
+  {"name": "La Pintada", "lat": 5.7483, "lon": -75.6054},
+  {"name": "Amagá", "lat": 6.0413, "lon": -75.7054},
+  {"name": "Santa Bárbara", "lat": 5.8825, "lon": -75.5676},
+  {"name": "Venecia", "lat": 5.9608, "lon": -75.7421},
+  {"name": "Abejorral", "lat": 5.7894, "lon": -75.4281},
+  {"name": "El Retiro", "lat": 6.0585, "lon": -75.5027},
+  {"name": "San Rafael", "lat": 6.2946, "lon": -75.0247}
+]
